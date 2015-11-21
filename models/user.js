ca578e73f5ab06c12cfb9af42f61329583f97723
Michael Jungo <michael.jungo@unifr.ch>
2015-11-21 11:49:31 +0100

    hash password and require username and password

--- models/user.js
+++ models/user.js
@@ -1,13 +1,44 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-  username: String,
-  password: String,
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
   email: String,
   firstName: String,
   lastName: String
 });
+
+// Update password
+UserSchema.pre('save', function(callback) {
+  var user = this;
+  if (!user.isModified('password')) return callback();
+
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) return callback(err);
+
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) return callback(err);
+      user.password = hash;
+      callback();
+    });
+  });
+});
+
+UserSchema.methods.verifyPassword = function(password, callback) {
+  bcrypt.compare(password, this.password, function(err, isMatch) {
+    if (err) return callback(err);
+    callback(null, isMatch);
+  });
+};
 
 module.exports = mongoose.model('User', UserSchema);
 
