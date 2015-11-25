a1946317e2d150ebdc75bd7084fde1fc53b176e8
Michael Jungo <michael.jungo@unifr.ch>
2015-11-25 11:58:06 +0100

    allow CORS properly

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var cors = require('cors');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -20,20 +21,6 @@ mongoose.connect(uristring, function (err, res) {
   }
 });
 
-var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, Content-Length, X-Requested-With');
-
-  // intercept OPTIONS method
-  if ('OPTIONS' == req.method) {
-    res.sendStatus(200);
-  }
-  else {
-    next();
-  }
-};
-
 var app = express();
 var api = require('./routes/api');
 
@@ -43,9 +30,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use('/api', api);
 app.use(passport.initialize());
-app.use(allowCrossDomain);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
