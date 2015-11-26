c443a89ee7023e23577d19364b9cfc860e1c00d5
Michael Jungo <michael.jungo@unifr.ch>
2015-11-26 18:17:55 +0100

    add some semicolons

--- routes/api.js
+++ routes/api.js
@@ -17,55 +17,55 @@ router.route('/authenticate')
 /* User routes */
 router.route('/users')
   .post(function(req, res, next) {
-    users.addUser(req, res, next)
+    users.addUser(req, res, next);
   })
   .get(function(req, res, next) {
-    users.getUsers(req, res, next)
+    users.getUsers(req, res, next);
   });
 
 /* Single user routes */
 router.route('/users/:user_id')
   .get(function(req, res, next) {
     if (!isIdValid(req.params.user_id)) return res.sendStatus(404);
-    users.getUser(req, res, req.params.user_id, next)
+    users.getUser(req, res, req.params.user_id, next);
   })
   .put(auth.authenticate, function(req, res, next) {
     if (!isIdValid(req.params.user_id)) return res.sendStatus(404);
-    users.updateUser(req, res, req.params.user_id, next)
+    users.updateUser(req, res, req.params.user_id, next);
   })
   .delete(auth.authenticate, function(req, res, next) {
     if (!isIdValid(req.params.user_id)) return res.sendStatus(404);
-    users.deleteUser(req, res, req.params.user_id, next)
+    users.deleteUser(req, res, req.params.user_id, next);
   });
 
 /* Project routes */
 router.route('/projects')
   .post(auth.authenticate, function(req, res, next) {
-    projects.addProject(req,res, next)
+    projects.addProject(req, res, next);
   })
   .get(auth.authenticate, function(req, res, next) {
-    projects.getProjects(req,res, next)
+    projects.getProjects(req, res, next);
   });
 
 /* Single project routes */
 router.route('/projects/:project_id')
   .get(auth.authenticate, function(req, res, next) {
     if (!isIdValid(req.params.project_id)) return res.sendStatus(404);
-    projects.getProject(req, res, req.params.project_id, next)
+    projects.getProject(req, res, req.params.project_id, next);
   })
   .put(auth.authenticate, function(req, res, next) {
     if (!isIdValid(req.params.project_id)) return res.sendStatus(404);
-    projects.updateProject(req, res, req.params.project_id, next)
+    projects.updateProject(req, res, req.params.project_id, next);
   })
   .delete(auth.authenticate, function(req, res, next) {
     if (!isIdValid(req.params.project_id)) return res.sendStatus(404);
-    projects.deleteProject(req, res, req.params.project_id, next)
+    projects.deleteProject(req, res, req.params.project_id, next);
   });
 
 router.route('/projects/:project_id/join')
   .post(auth.authenticate, function(req, res, next) {
     if (!isIdValid(req.params.project_id)) return res.sendStatus(404);
-    projects.joinProject(req, res, req.params.project_id, next)
+    projects.joinProject(req, res, req.params.project_id, next);
   });
 
 router.route('/projects/:project_id/leave')
