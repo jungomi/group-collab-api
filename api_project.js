36527c05b34d45ddc423df9431314b7ba794f1d3
Michael Jungo <michael.jungo@unifr.ch>
2015-12-17 22:24:17 +0100

    initial commit

---
+++ api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "group-collab-api",
+  "description": "Group Collaboration RESTful API",
+  "title": "Group Collaboration API",
+  "version": "0.0.0",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2015-12-17T21:11:40.298Z",
+    "url": "http://apidocjs.com",
+    "version": "0.14.0"
+  }
+});
